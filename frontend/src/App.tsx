a43d0682ac9676dbbe4ced357f7bb2fb3b6eefa9
wallanpsantos <32087983+wallanpsantos@users.noreply.github.com>
2021-09-07 14:51:30 -0300

    Adicionando componentes basicos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 function App(): JSX.Element {
   return (
-    <div >
-      <h1 className="text-primary">Salve!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Salve!</h1>
+      </div>
+      <DataTable />
+      <Footer />
+    </>
   );
 }
 
