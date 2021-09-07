6fb9fac066ad59512159bc4f165f02daa5a5e6b6
wallanpsantos <32087983+wallanpsantos@users.noreply.github.com>
2021-09-07 16:42:33 -0300

    Teste de CI CD

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ function Footer(): JSX.Element {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://github.com/wallanpsantos" target="_blank" rel="noreferrer">Wallan Pereira</a></p>
+                <p className="text-light">App desenvolvido por <a href="https://github.com/wallanpsantos" target="_blank" rel="noreferrer">Wallan Pereira | Flavia</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
                     Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
