e9a253b63de57af624e9174139ea11c13dacba33
wallanpsantos <32087983+wallanpsantos@users.noreply.github.com>
2021-09-12 14:36:36 -0300

    Adicionado Link para dashboard

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,9 +1,22 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import { Link } from "react-router-dom";
+
 function Home(): JSX.Element {
     return (
-        <div className="">
-            <h1>Página Home</h1>
-        </div>
-
+        <>
+            <NavBar />
+            <div className="container">
+                <div className="jumbotron">
+                    <h1 className="display-4">DSVendas</h1>
+                    <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+                    <hr />
+                    <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+                </div>
+                <Link to="/dashboard">Acessar dashboard</Link>
+            </div>
+            <Footer />
+        </>
     );
 }
 
