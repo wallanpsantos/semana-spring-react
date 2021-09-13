add6b0b222f9deffa0beb335acd4e92cd9ff39cd
wallanpsantos <32087983+wallanpsantos@users.noreply.github.com>
2021-09-12 23:35:26 -0300

    BarChart integration

--- backend/src/main/java/com/org/backend/dto/SaleSuccessDTO.java
+++ backend/src/main/java/com/org/backend/dto/SaleSuccessDTO.java
@@ -16,12 +16,12 @@ public class SaleSuccessDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    private String sellerNamename;
+    private String sellerName;
     private Long visited;
     private Long deals;
 
     public SaleSuccessDTO(Seller seller, Long visited, Long deals) {
-        this.sellerNamename = seller.getName();
+        this.sellerName = seller.getName();
         this.visited = visited;
         this.deals = deals;
     }
